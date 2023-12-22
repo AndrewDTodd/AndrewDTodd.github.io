203e66da8a53da6a76bea1dd9f898214077a156f
AndrewDTodd <44379289+AndrewDTodd@users.noreply.github.com>
2023-12-22 07:28:52 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', function () {
+	const business_card = document.getElementById('hoverBusinessCard');
+	let isFlipped = false;
+	let originalTransform = 'none';
+	let defaultBoxShadow;
+
+	function toggleImage() {
+		isFlipped = !isFlipped;
+
+		originalTransform = getComputedStyle(business_card).transform;
+
+		business_card.style.transform = originalTransform + 'scaleY(0)';
+
+		setTimeout(function () {
+			business_card.src = isFlipped ? 'Images/BusinessCard_back.png' : 'Images/BusinessCard_front.png';
+		
+			business_card.style.transform = originalTransform;
+		}, 300);
+	}
+
+	business_card.addEventListener('click', toggleImage);
+
+	business_card.addEventListener('mouseenter', function () {
+		business_card.style.transition = 'transform 0.3s ease-out, box-shadow 0.3s ease-out';
+
+		defaultBoxShadow = getComputedStyle(business_card).boxShadow;
+	});
+
+	business_card.addEventListener('mousemove', function (event) {
+		const imageRect = business_card.getBoundingClientRect();
+		const mouseXPercent = ((event.clientX - imageRect.left) / imageRect.width) * 100;
+		const mouseYPercent = ((event.clientY - imageRect.top) / imageRect.height) * 100;
+
+		const rotateX = (mouseXPercent - 50) * 0.2;
+		const rotateY = (mouseYPercent - 50) * 0.2;
+
+		business_card.style.transform = 'perspective(900px) rotateX(' + rotateY + 'deg) rotateY(' + -rotateX + 'deg) scale(1.1, 1.1)';
+
+		const shadowSize = 10 + Math.abs(rotateX) + Math.abs(rotateY);
+		const shadowX = rotateY * 2;
+		const shadowY = rotateX * 2;
+
+		business_card.style.boxShadow = shadowY + 'px ' + shadowX + 'px ' + shadowSize + 'px rgba(0, 0, 0, 0.5)';
+	});
+
+	business_card.addEventListener('mouseleave', function () {
+		business_card.style.transform = 'none';
+
+		business_card.style.boxShadow = defaultBoxShadow;
+	});
+});
