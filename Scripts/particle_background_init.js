09d83330f80bc069c50642550e03556b75ab44b4
Andrew Todd <andrewdanieltodd@gmail.com>
2023-12-23 08:48:25 -0500

    Update to website. Header/title section has most elements

--- Scripts/particle_background_init.js
+++ Scripts/particle_background_init.js
@@ -2,6 +2,11 @@ window.onload = function()
 {
   Particles.init({
       selector: '.particles-background',
-      connectParticles: 'true',
+      maxParticles: '95',
+      sizeVariations: '1',
+      speed: '0.4',
+      color: '#c9e8f0',
+      minDistance: '110',
+      connectParticles: 'true'
   });
 };
