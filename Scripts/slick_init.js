3dd3ed82f96ca636bfc7be4d2db8c92dbf79b9b0
Andrew Todd <andrewdanieltodd@gmail.com>
2024-01-02 02:05:43 -0500

    Update to the 3d modeling exhibit's carousel. Seemes to finally be configured properly to achieve the desired effect

---
+++ Scripts/slick_init.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        arrows: true,
+        dots: true,
+        //centerMode: true,
+        //centerPadding: '0',
+        slidesToShow: 1
+    });
+});
