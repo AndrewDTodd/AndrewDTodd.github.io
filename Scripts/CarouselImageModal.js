b8379d5868e909ed2a496cf2dd356ed61494b9fb
andrewdanieltodd@gmail.com <andrewdanieltodd@gmail.com>
2024-01-24 04:06:06 -0700

    Initial version of the 3D Models Catalog is done. Added three of the models to the exhibit on the home page

--- Scripts/CarouselImageModal.js
+++ Scripts/CarouselImageModal.js
@@ -1,15 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
     const modal = document.getElementById('carousel-image-modal');
 
-    const carouselImages = document.querySelectorAll('.carousel img');
+    const carouselImages = document.querySelectorAll('.catalog-item img');
 
     const modalImage = document.getElementById('image-modal-image');
 
+    let clickStartTIme;
     carouselImages.forEach(function(img) {
-        img.addEventListener('click', function() {
-            modal.style.display = 'flex';
-            modalImage.src = this.src;
+        img.addEventListener('mousedown', function(event) {
+            clickStartTIme = Date.now();
         });
+
+        img.addEventListener('mouseup', function(event) {
+            const clickDuration = Date.now() - clickStartTIme;
+
+            if(clickDuration < 200) {
+                modal.style.display = 'flex';
+                modalImage.src = this.src;
+            }
+
+            clickStartTIme = null;
+        });
+
+        img.addEventListener('dragStart', function(event) {
+            event.preventDefault();
+        })
     });
 
     const closeBtn = document.getElementsByClassName('image-modal-close')[0];
@@ -18,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     window.addEventListener('click', function(event) {
-        if(event.target === modal) {
+        if(event.target === modalImage) {
             modal.style.display = 'none';
         }
     });
